a140a752e03ac34634ddf69ae4090865769be88f
kylejaster <kyle@rayogram.com>
2011-02-28 23:52:58 -0500

    ok - basic setup working - just dummy content from jquery mobile framework - next up? CiviAPIgit status

--- templates/civimobile.page.tpl.php
+++ templates/civimobile.page.tpl.php
@@ -2,24 +2,18 @@
 // $Id: page.tpl.php,v 1.0 kylejaster Exp $ ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $civimobile_page_settings['language'] ?>" lang="<?php print $civimobile_page_settings['language'] ?>" >
-<head>
-  <title><?php print $head_title ?></title>
-  <meta http-equiv="content-language" content="<?php print $language->language ?>" />
-  <?php print $meta; ?>
-  <?php print $head; ?>
-  <?php print $styles; ?>
-  <!--[if lte IE 7]>
-    <link rel="stylesheet" href="<?php print $path_parent; ?>framework/ie.css" type="text/css" media="screen, projection">
-    <link href="<?php print $path_parent; ?>css/ie.css" rel="stylesheet"  type="text/css"  media="screen, projection" />
-    <?php $styles_ie_rtl['ie']; ?>
-  <![endif]-->
-  <!--[if lte IE 6]>
-    <link href="<?php print $path_parent; ?>css/ie6.css" rel="stylesheet"  type="text/css"  media="screen, projection" />
-    <?php $styles_ie_rtl['ie6']; ?>
-  <![endif]-->
-  <?php print $scripts ?>
+  <head>
+    <title><?php print $civimobile_page_settings['title'] ?></title>
+    <?php print $civimobile_page_settings['head'] ?>
+    <?php print $civimobile_page_settings['favicon'] ?>
+    <?php print $civimobile_page_settings['styles'] ?>
+    <?php print $civimobile_page_settings['scripts'] ?>
 
-</head>
+    <link rel="stylesheet" href="http://code.jquery.com/mobile/1.0a3/jquery.mobile-1.0a3.min.css" />
+    <script src="http://code.jquery.com/jquery-1.5.min.js"></script>
+    <script src="http://code.jquery.com/mobile/1.0a3/jquery.mobile-1.0a3.min.js"></script>
+    
+  </head>
 <body> 
 <div data-role="page" data-theme="b" id="jqm-home"> 
 	<div id="jqm-homeheader"> 
