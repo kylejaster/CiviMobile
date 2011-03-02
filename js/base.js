bf1baf40cef117d894fddbd7f56359818cfdb15a
kylejaster <kyle@rayogram.com>
2011-03-02 00:27:22 -0500

    pulling list of participants working based on url param for event_id

--- js/base.js
+++ js/base.js
@@ -1,34 +1,100 @@
+function getUrlVars() {
+			var vars = [], hash;
+			var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+			for(var i = 0; i < hashes.length; i++) {
+				hash = hashes[i].split('=');
+				vars.push(hash[0]);
+				vars[hash[0]] = hash[1];
+			}
+			return vars;
+		}
 
 
-// generate list of events
-$().crmAPI ('Event','get',{'version' :'3' }
-  ,{ 
-    ajaxURL: crmajaxURL,
-    success:function (data){
-      $('#home-content').append('<ul id="events-list" data-role="listview" data-inset="true"></ul>');
-      $.each(data.values, function(key, value) {
-        $('#events-list').append('<li role="option" tabindex="-1" data-theme="c" id="event-'+value.id+'"><a href="#'+value.id+'">'+value.title+'</a></li>');
-        });
-      $('#events-list').listview();
-      },
-   }
-);
+        // generate list of events
+        $().crmAPI ('Event','get',{'version' :'3' }
+          ,{ 
+            ajaxURL: crmajaxURL,
+            success:function (data){
+              $('#home-content').append('<ul id="events-list" data-role="listview" data-inset="true"></ul>');
+              $.each(data.values, function(key, value) {
+                $('#events-list').append('<li role="option" tabindex="-1" data-theme="c" id="event-'+value.id+'"><a href="civimobile/participants&event_id='+value.id+'">'+value.title+'</a></li>');
+                });
+              $('#events-list').listview();
+              },
+           }
+        );
 
 
-// listen for click on event
-// then generate participants page
+$(function(){
+
+	$('div').live('pageshow',function(event, ui){
+
+        console.log(getUrlVars());
+        
+        urlVars = getUrlVars();
+        console.log(urlVars[0][1]);
+        
+
+        
+        
+        // listen for click on event
+        // then generate participants page
+        if (urlVars[0][0]){
+             $().crmAPI ('Participant','get',{'version' :'3', 'event_id' : urlVars[0][0 ] }
+               ,{ 
+                   ajaxURL: crmajaxURL,
+                   success:function (data){    
+                   $.each(data.values, function(key, value) {
+                     $('#participants-list').append('<li role="option" tabindex="-1" data-theme="c"><a href="#">'+value.display_name+'</a></div></li>');        
+                     });
+                   $('#participants-list').listview();
+                   },
+                 });
+            }
+        
+function dump(arr,level) {
+	var dumped_text = "";
+	if(!level) level = 0;
+	
+	//The padding given at the beginning of the line.
+	var level_padding = "";
+	for(var j=0;j<level+1;j++) level_padding += "    ";
+	
+	if(typeof(arr) == 'object') { //Array/Hashes/Objects 
+		for(var item in arr) {
+			var value = arr[item];
+			
+			if(typeof(value) == 'object') { //If it is an array,
+				dumped_text += level_padding + "'" + item + "' ...\n";
+				dumped_text += dump(value,level+1);
+			} else {
+				dumped_text += level_padding + "'" + item + "' => \"" + value + "\"\n";
+			}
+		}
+	} else { //Stings/Chars/Numbers etc.
+		dumped_text = "===>"+arr+"<===("+typeof(arr)+")";
+	}
+	return dumped_text;
+ };
+        
+        
+		function getUrlVars() {
+			var vars = [], hash;
+			var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+			for(var i = 1; i < hashes.length; i++) {
+				hash = hashes[i].split('=');
+				vars.push([hash[0], hash[1]]);
+				//vars[hash[0]] = hash[1];
+			}
+			return vars;
+		}
+
+	});
+
+});
 
 
-$().crmAPI ('Participant','get',{'version' :'3', 'event_id' : '1' }
-  ,{ 
-      ajaxURL: crmajaxURL,
-      success:function (data){    
-      $.each(data.values, function(key, value) {
-        $('#jqm-events .ui-listview').append('<li role="option" tabindex="-1" data-theme="c"><a href="#">'+value.display_name+'</a></div></li>');        
-        });
-      $('#jqm-events .ui-listview').listview('refresh');
-      },
-    });
+
 
 /*
 $().crmAPI ('participant','update',{id:id,status_id:toStatus}, 
